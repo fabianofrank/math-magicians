69be6fcd9883bc0fa3bf4f135f611124210f0598
fabianofrank <fabianofrank@protonmail.com>
2022-01-14 08:24:06 -0300

    Add event handler

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,5 @@
 import React from 'react';
-// import calculate from '../logic/calculate';
-// import operate from '../logic/operate';
+import calculate from '../logic/calculate';
 
 const buttons = [
   'AC', '+/-', '%', '÷',
@@ -10,22 +9,42 @@ const buttons = [
   '0', '.', '=',
 ];
 
-const Calculator = () => {
-  const clickHandler = () => {};
-  const buttonsArray = [];
-  buttons.forEach((button) => buttonsArray.push(
-    <button click={clickHandler} className="calc-buttons" type="button">{button}</button>,
-  ));
-  return (
-    <div id="calculator">
-      <div id="calc-display">
-        <input />
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.handleClick = this.handleClick.bind(this);
+    this.handleEvent = this.handleEvent.bind(this);
+    this.buildArray = this.buildArray.bind(this);
+    this.state = { total: '0', next: null, operation: null };
+  }
+
+  handleEvent = (e) => {
+    this.setState((previousState) => calculate(previousState, e.target.innerText));
+  }
+
+  buildArray = () => {
+    const buttonsArray = [];
+    buttons.forEach((button) => buttonsArray.push(
+      <button key={button} onClick={this.handleEvent} className="calc-buttons" type="button">{button}</button>,
+    ));
+    return buttonsArray;
+  };
+
+  render() {
+    const { total, next, operation } = this.state;
+    return (
+      <div id="calculator">
+        <div id="calc-display">
+          {total}
+          {operation}
+          {next}
+        </div>
+        <div id="calc-buttons">
+          {this.buildArray()}
+        </div>
       </div>
-      <div id="calc-buttons">
-        {buttonsArray}
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Calculator;
